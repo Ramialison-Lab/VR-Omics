cb2021d99871f9ce2131ee386e71c7d018f59ad4
Denis Bienroth <denisbienroth@gmail.com>
2022-06-30 13:59:09 +1000

    menu update

---
+++ Assets/Scripts/Visualisation/TooltipManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class TooltipManager : MonoBehaviour
+{
+
+    public string tooltipText = "";
+
+    private void OnMouseEnter()
+    {
+        //TBD add tooltip
+    }
+}
