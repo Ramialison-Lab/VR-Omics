0dfd3327836aae78683752e13d220bde3f5240d1
Denis Bienroth <denisbienroth@gmail.com>
2022-08-17 10:58:23 +1000

    Pipeline

---
+++ Assets/Scripts/Pipeline/DataTransfer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataTransfer : MonoBehaviour
+{
+    public bool visium =false;
+    public bool c18 = false;
+    public bool xenium = false;
+    public bool tomoseq = false;
+
+    public void setTEchnology(string str) {
+
+        switch (str)
+        {
+
+            case "visium": visium = true;
+                break;
+            case "c18": c18 = true;
+                break;
+            case "xenium": xenium = true;
+                break;
+            case "tomoseq": tomoseq = true;
+                break;
+        }
+
+    }
+
+
+    public void startC18()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
